50055d1533dc4b2ba87007b3180a5244151a8bbe
Archita21 <35433234+Archita21@users.noreply.github.com>
2018-01-16 00:46:19 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+ printf("Enter the no");
+ scanf("%d",&n);
+ 
+ if(n%2==0)
+ {
+ printf("the no is odd");
+}
+ else
+ {
+ printf("the no is even");
+}
+
+return 0;
+}
